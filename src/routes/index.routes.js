c852d2824c1772da4ba1eb15be178604bbae3499
vipin760 <vipinm500@gmail.com>
2026-01-29 11:21:53 +0530

    index file update

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,5 @@
 export default async function indexRoutes(fastify) {
  fastify.get("/",(req,res)=>{
-   res.status(200).send({status:true,message:"server running successfully"})
+   res.status(200).send({status:true,message:"server running successfully update"})
  })
 }
